3442de1342b80b4e68c10b7c3d79500deb487c33
VaishnaviYadav9623 <anilyadav98532@gmail.com>
2025-09-09 15:51:02 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,7 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30}; 
+            System.out.println("Number at index 3: " + numbers[3]);
+       }
+}
+
